d048205563c6f1500b95a81eccc1238b5b6af0ef
Sephylon98 <Sephylon1998@gmail.com>
2023-08-23 00:23:47 +0300

    [Week1: Friday session, Date: 2023/08/04]: Uploading Tasks

--- task3/task3_env_variables.py
+++
@@ -1,6 +0,0 @@
-#Write a python program to access environment variables.
-
-import os
-
-for key,value in os.environ.items():
-    print("{}:{}".format(key,value))
--- task4/task4_area_of_circle.py
+++
@@ -1,7 +0,0 @@
-from math import pi
-
-Radius = int(input("Enter Radius of Circle: "))
-
-Area = Radius**2 * pi
-
-print(Area)
